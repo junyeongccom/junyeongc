4f56c002628aff02357adcd53881ce1c77010b05
junyoungccom <junyeongc1000@gmail.com>
2025-01-31 18:10:04 +0900

    폼태그 작성 2025-01-31 18:09

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def login():
+    return render_template("auth/login.html")
+
+@app.route('/index')
 def index():
     return render_template("index.html")
 
