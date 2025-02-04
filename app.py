f036dce2fe4d8ea7781e08ff4ae7848c1be31740
junyoungccom <junyeongc1000@gmail.com>
2025-02-04 20:45:31 +0900

    계산기 알고리즘 스토리지 축소 성공! 2025-02-04 20:45

--- app.py
+++ app.py
@@ -11,21 +11,6 @@ def index():
     print("😘홈페이지로 이동")
     return render_template("index.html")
 
-@app.route('/minus')
-def minus():
-    return render_template("calculator/minus.html")
-
-@app.route('/plus')
-def plus():
-    return render_template("calculator/plus.html")
-
-@app.route('/multiple')
-def multiple():
-    return render_template("calculator/multiple.html")
-
-@app.route('/divide')
-def divide():
-    return render_template("calculator/divide.html")
 
 @app.route('/manufincheck')
 def manufincheck():
@@ -78,57 +63,35 @@ def login():
         return render_template("auth/loginfail.html")
 
     
-@app.route('/plusanswer',methods=["POST"])
-def plusanswer():
-    print("➕덧셈 알고리즘")
-    num1 = request.form.get('num1')
-    num2 = request.form.get('num2')
-    print("num1:", num1)
-    print("num2:", num2)
-    result = int(num1) + int(num2)
-    print("결과:", num1, "+", num2, "=", result)
-    print("🤓덧셈 성공")
+@app.route('/calc',methods=["POST", "GET"])
+def calc(): 
+    if request.method == "POST":
+        print("😊post방식으로 전송한 데이터😊")
+        print("🎃계산기 알고리즘🎃")
+        num1 = request.form.get('num1')
+        opcode = request.form.get('opcode')
+        num2 = request.form.get('num2')
+        print("num1:", num1)
+        print("num2:", num2)
+        print("opcode:", opcode)
+        if opcode == '+':
+            result = int(num1) + int(num2)
+        elif opcode == '-':
+            result = int(num1) - int(num2)
+        elif opcode == '*':
+            result = int(num1) * int(num2)
+        elif opcode == '/':
+            result = int(num1) / int(num2)
+        else:
+            print("❗연산자 오류 발생❗")
+            result = "유효한 연산자가 아닙니다. (+, -, *, / 만 사용 가능)"
+            
+        print(num1, opcode, num2, "=", result)
+        return render_template("/calculator/calc.html", num1 = num1, opcode=opcode, num2 = num2, result = result)
+    else: 
+        print("😊get방식으로 전송한 데이터😊")
+        return render_template("/calculator/calc.html")
 
-    return render_template("answer/plus.html", num1 = num1, num2 = num2, result = result)
-
-@app.route('/minusanswer',methods=["POST"])
-def minusanswer():
-    print("➖뺄셈 알고리즘")
-    num1 = request.form.get('num1')
-    num2 = request.form.get('num2')
-    print("num1:", num1)
-    print("num2:", num2)
-    result = int(num1) - int(num2)
-    print("결과:", num1, "-", num2, "=", result)
-    print("🤓뺄셈 성공")
-
-    return render_template("answer/minus.html", num1 = num1, num2 = num2, result = result)
-
-@app.route('/divideanswer',methods=["POST"])
-def divideanswer():
-    print("➗나눗셈 알고리즘")
-    num1 = request.form.get('num1')
-    num2 = request.form.get('num2')
-    print("num1:", num1)
-    print("num2:", num2)
-    result = int(num1) / int(num2)
-    print("결과:", num1, "/", num2, "=", result)
-    print("🤓나눗셈 성공")
-
-    return render_template("answer/divide.html", num1 = num1, num2 = num2, result = result)
-
-@app.route('/multipleanswer',methods=["POST"])
-def multipleanswer():
-    print("✖️곱셈 알고리즘")
-    num1 = request.form.get('num1')
-    num2 = request.form.get('num2')
-    print("num1:", num1)
-    print("num2:", num2)
-    result = int(num1) * int(num2)
-    print("결과:", num1, "*", num2, "=", result)
-    print("🤓곱셈 성공")
-
-    return render_template("answer/multiple.html", num1 = num1, num2 = num2, result = result)
 
 if __name__ == '__main__ ' :
    app.run(debug=True)
